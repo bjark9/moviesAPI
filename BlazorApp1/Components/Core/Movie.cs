e1b896aec273285ea4615bcbc074e83fb7d63550
Niels Martin <niels.martin03@gmail.com>
2026-03-02 18:23:42 +0100

    Save before resuming

---
+++ BlazorApp1/Components/Core/Movie.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp1.Components.Core
+{
+    public class Movie
+    {
+        public string Title {get;set;}
+        public string TagLine {get;set;}
+        public float Popularity {get;set;}
+        public string Overview {get;set;}
+    }
+}
--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient(); // Add this for the API requests
 
 var app = builder.Build();
 
