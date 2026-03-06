8d105f75a7cf544816cab466dccc292414aac1a6
Niels Martin <niels.martin03@gmail.com>
2026-03-06 13:37:28 +0100

    Can now remove movies from favorites

--- BlazorApp1/Components/Core/FavoritesState.cs
+++ BlazorApp1/Components/Core/FavoritesState.cs
@@ -2,8 +2,8 @@ using BlazorApp1.Components.Core;
 
 public class FavoritesState
 {
-    private List<Movie> favorites = new();
-    public IReadOnlyList<Movie> Favorites => favorites;
+    private List<Movie> favorites = new(); // Only accessible from inside FavoritesState
+    public IReadOnlyList<Movie> Favorites => favorites; // Accessible from everywhere in Readonly 
     public event Action? OnChange;
     public void AddToFavorites(Movie movie)
     {
